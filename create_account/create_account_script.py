51788eb24cc78d7dad7bfc993e29145f71893a71
BlueRyse <57491067+BlueRyse@users.noreply.github.com>
2021-10-19 16:33:18 +0200

    Add files via upload

---
+++ create_account/create_account_script.py
@@ -0,0 +1,14 @@
+import create_account
+from stellar_sdk.server import Server
+
+server = Server("https://horizon-testnet.stellar.org")
+genesis_account = "SCWUTKBTSPKETXBAIFOIK6ZY33AJILNDGJTIN7J5QK7JCZBUFDPFCQKS"
+new_account_public_ID = input("Please enter the new account Public ID: ")
+balance = input("Please enter new account starting balance: ")
+
+account = create_account.create_account(server,genesis_account,new_account_public_ID,balance)
+
+print(f"Transaction hash: {account.response['hash']}")
+print(
+    f"Account created: \n\taccount id: {new_account_public_ID}"
+)
