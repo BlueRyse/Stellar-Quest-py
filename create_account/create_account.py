a4309f4484fe5521c1df7534a7e6ff0240abad5f
BlueRyse <57491067+BlueRyse@users.noreply.github.com>
2021-10-19 18:24:36 +0200

    fixed a grammar mistake on comment

--- create_account/create_account.py
+++ create_account/create_account.py
@@ -1,6 +1,6 @@
 from stellar_sdk import Keypair, Network, Server, TransactionBuilder
 
-#This class permits to create an account on testnet or mainnet, used on create_account_script.py and create_random_account_script.py
+#This class allows to create an account on testnet or mainnet, used on create_account_script.py and create_random_account_script.py
 
 class create_account:
 
