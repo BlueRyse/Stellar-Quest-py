e3f052464515055247da3f72628d829fae594731
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-20 15:24:09 +0200

    clearer console message

--- payment/payment_script.py
+++ payment/payment_script.py
@@ -2,7 +2,7 @@ import payment
 from stellar_sdk import Keypair
 
 source_secret = "your source secret seed"
-dest_account = input("Please, insert the destination account: ")
+dest_account = input("Please, insert the destination account public key: ")
 amount = input("How many lumens would you like to send: ")
 
 pay = payment.payment(source_secret, dest_account, amount, "XLM")
