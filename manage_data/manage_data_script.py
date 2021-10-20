b61a2bcfc3b04b81953c1d00720dfaafb9e399df
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-20 22:43:21 +0200

    Clearer output

--- manage_data/manage_data_script.py
+++ manage_data/manage_data_script.py
@@ -5,4 +5,4 @@ data_name = input("Insert the data name: ")
 data_value = input("Insert the data value: ")
 manage_data_op = manage_data.manage_data(source_secret,data_name,data_value)
 
-print("Set: " + data_name + " " + data_value + " for " + manage_data_op.source.public_key)
+print("Setted: " + data_name + " " + data_value + " for " + manage_data_op.source.public_key)
