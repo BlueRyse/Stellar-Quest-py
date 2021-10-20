1689fe08e0594082ac7a52580e437fd1e797b872
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-20 23:59:02 +0200

    fixed English

--- payment/payment_script.py
+++ payment/payment_script.py
@@ -1,8 +1,8 @@
 import payment
 
-source_secret = "your source secret" 
+source_secret = "your source secret"
 dest_account = input("Please, insert the destination account public key: ")
-asset = input("What asset you want to send? Type XLM if you want to send lumens ")
+asset = input("What asset do you want to send? Type XLM if you want to send lumens ")
 if asset != "XLM":
     asset_issuer = input("Insert the asset issuer public key: ")
     amount = input("How many " + asset + " would you like to send: ")
