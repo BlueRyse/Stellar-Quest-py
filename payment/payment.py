56fcfaa7089c21b7e88bf0926909e909144bf83d
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-20 13:35:03 +0200

    create_transcation and execute_transaction now private. Added fee parameter for custom fee on payment_script.py

--- payment/payment.py
+++ payment/payment.py
@@ -1,22 +1,22 @@
 from stellar_sdk import Keypair, Network, Server, TransactionBuilder
 
 class payment:
-    def __init__(self, source_secr_seed, new_acc_public_key, amount, asset):
+    def __init__(self, source_secr_seed, new_acc_public_key, amount, asset, fee = 100):
         self.asset = asset
-        self.mbase_fee = 100
+        self.fee = fee
         self.server = Server("https://horizon-testnet.stellar.org")
         self.source = Keypair.from_secret(source_secr_seed)
         self.source_account = self.server.load_account(account_id=self.source.public_key)
         self.dest_account = Keypair.from_public_key(new_acc_public_key)
         self.amount = amount
-        self.create_transaction()
+        self.__create_transaction()
 
-    def create_transaction(self):
+    def __create_transaction(self):
         self.transaction = (
             TransactionBuilder(
                 source_account = self.source_account,
                 network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
-                base_fee = self.mbase_fee
+                base_fee = self.fee
             )
          .add_text_memo("Test_transaction")
          .append_payment_op(
@@ -27,10 +27,8 @@ class payment:
          .set_timeout(30)  # Make this transaction valid for the next 30 seconds only
          .build()
         )
-        self.execute_transaction()
+        self.__execute_transaction()
 
-    def execute_transaction(self):
-        if self.transaction is None:
-            raise ValueError("Create transaction first")
+    def __execute_transaction(self):
         self.transaction.sign(self.source)
         self.response = self.server.submit_transaction(self.transaction)
