f3e95f54d2d58581f4648102a013a8a8ce704287
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-20 22:31:58 +0200

    Possibility to input custom assets

--- payment/payment_script.py
+++ payment/payment_script.py
@@ -3,7 +3,7 @@ import payment
 source_secret = "your secret seed"
 dest_account = input("Please, insert the destination account public key: ")
 amount = input("How many lumens would you like to send: ")
-
-pay = payment.payment(source_secret, dest_account, amount, "XLM")
+asset = input("What asset you want to send? Type XLM if you want to send lumens")
+pay = payment.payment(source_secret, dest_account, amount, asset)
 
 print("Sent " + amount + " XLM \nfrom " + pay.source.public_key + "\nto " + dest_account)
