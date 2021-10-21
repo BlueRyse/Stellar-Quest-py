4d694a52a847ab784ad1ada8bca3b4f195eec44e
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-21 17:40:24 +0200

    asset input and asset code are now inputs

--- change_trust/change_trust_script.py
+++ change_trust/change_trust_script.py
@@ -1,7 +1,7 @@
 import change_trust
 
-source = "source secret seed" #the account that wants to receive the asset
-asset_issuer = "issuer public key"
-asset_code="testXLM" #your asset code
+source = "your source secret seed" #the account that wants to receive the asset
+asset_issuer = input("Insert the issuer public key: ")
+asset_code = input("Insert the asset code: ") #your asset code
 
 change_trust.change_trust(asset_issuer, asset_code,source,"100") #100 is a test limit
