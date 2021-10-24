2d7a81b26177664fa4c8f21a27d7cb6ffa18e149
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-24 21:43:49 +0200

    Output message improvement

--- payment/payment_script.py
+++ payment/payment_script.py
@@ -2,7 +2,7 @@ import payment
 
 source_secret = input("Please, insert your source secret seed: ")
 dest_account = input("Please, insert the destination account public key: ")
-asset = input("What asset do you want to send? Type XLM if you want to send lumens ")
+asset = input("What asset do you want to send? Type XLM if you want to send lumens: ")
 if asset != "XLM":
     asset_issuer = input("Insert the asset issuer public key: ")
     amount = input("How many " + asset + " would you like to send: ")
