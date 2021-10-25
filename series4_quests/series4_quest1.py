31bde583c9cdc0f9681598a3d5a3322b2b255c10
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-25 18:18:27 +0200

    series 4 quest 1 solution

---
+++ series4_quests/series4_quest1.py
@@ -0,0 +1,31 @@
+#has an example try to add added a :pray: emoji to this account a new random account with stellar laboratory
+#this account needs to have a threshold of 10 for ALL operations (like in SQ04:01). The new pray
+#emoji signer will need tohave a weight of 5.The master Weight will need to be 5 as well
+#This means that we need both to sign EVERY transaction (5 + 5 = 10)
+
+from stellar_sdk import Keypair, Network, Server, TransactionBuilder
+
+server = Server("https://horizon-testnet.stellar.org")
+
+source_secr_seed = "the account that holds the XLM" #the secret key, the first signer
+dest = 'your albedo account(if this was pubnet)' #the account that survives (all XLM will be here after merge)
+source = Keypair.from_secret(source_secr_seed)
+source_acc = server.load_account(account_id=source.public_key)
+str = ':pray:'.encode('utf-8')
+hex_signer = str.hex() #the String to hex, the second signer
+transaction = (
+    TransactionBuilder(
+        source_account = source_acc,
+        network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
+        base_fee = 100
+    )
+    .append_account_merge_op(
+    destination=dest
+    )
+    .build()
+)
+
+transaction.sign(source)
+transaction.sign_hashx(hex_signer)
+
+response = server.submit_transaction(transaction) #account merged, and now the XLM will be yours :D
