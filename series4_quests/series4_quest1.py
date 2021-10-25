def6e304cd9501dd79a390fe9b5c91b91b822017
Sergio Evola <Evola.sergio97@gmail.com>
2021-10-25 18:42:29 +0200

    Now it creates a random account with the emoji signer. Updated info.

--- series4_quests/series4_quest1.py
+++ series4_quests/series4_quest1.py
@@ -1,31 +1,63 @@
-#as an example try to add a :pray: emoji to a new random account with stellar laboratory
-#this account needs to have a threshold of 10 for ALL operations (like in SQ04:01). The new pray
-#emoji signer will need to have a weight of 5. The master Weight will need to be 5 as well.
+#as an example we add the pray emoji as a signer to a radonm account
+#this account has a threshold of 10 for ALL operations (like in SQ04:01). The new pray
+#emoji signer will have a weight of 5. The master Weight will also be 5 as well.
 #This means that we need both to sign EVERY transaction (5 + 5 = 10)
+#After I create a new random account with the emoji signer, I perform a merge account operation.
 
 from stellar_sdk import Keypair, Network, Server, TransactionBuilder
+import requests, hashlib
 
 server = Server("https://horizon-testnet.stellar.org")
 
-source_secr_seed = "the account that holds the XLM and has the :pray: emoji signer" #the secret key, the first signer
-dest = 'your albedo account(if this was pubnet)' #the account that survives (all XLM will be here after merge)
-source = Keypair.from_secret(source_secr_seed)
-source_acc = server.load_account(account_id=source.public_key)
-str = ':pray:'.encode('utf-8')
-hex_signer = str.hex() #the String to hex, the second signer
-transaction = (
+keypair = Keypair.random() #a random account that will have the emoji signer (like in SQ04:01)
+url = 'https://friendbot.stellar.org'
+response = requests.get(url, params={'addr': keypair.public_key})
+keypair_account = server.load_account(account_id=keypair.public_key)
+signer_string = ":pray:"
+sha256_signer = hashlib.sha256(signer_string.encode('utf-8')).hexdigest() #this will be added as a Signer
+
+add_hash_signer_transaction = (
     TransactionBuilder(
-        source_account = source_acc,
+        source_account = keypair_account,
+        network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
+        base_fee = 100
+    )
+.add_text_memo("Added hash signer")
+.append_set_options_op( #tresholds like in SQ04:01
+    master_weight = 5,
+    low_threshold = 10,
+    med_threshold = 10,
+    high_threshold = 10
+ )
+.append_hashx_signer(
+    sha256_hash = sha256_signer,
+    weight = 5
+  )
+  .set_timeout(30)  # Make this transaction valid for the next 30 seconds only
+  .build()
+)
+
+dest = 'GAM6Y6ZZ4AIPVYOSJWG7XEHPDP23VJRX2AULK72FGDHKS62MEXAUOBJW' #the account that survives (all XLM will be here after merge)
+str = signer_string.encode('utf-8')
+hex_signer = str.hex() #the String to hex, the second signer
+merge_transaction = (
+    TransactionBuilder(
+        source_account = keypair_account,
         network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
         base_fee = 100
     )
     .append_account_merge_op(
-    destination=dest
+        destination=dest
     )
     .build()
 )
 
-transaction.sign(source)
-transaction.sign_hashx(hex_signer)
+add_hash_signer_transaction.sign(keypair)
 
-response = server.submit_transaction(transaction) #account merged, and now the XLM will be yours :D
+response = server.submit_transaction(add_hash_signer_transaction)
+
+#we need to sign this one with both signers :)
+merge_transaction.sign(keypair)
+merge_transaction.sign_hashx(hex_signer)
+
+response1 = server.submit_transaction(merge_transaction) #account merged
